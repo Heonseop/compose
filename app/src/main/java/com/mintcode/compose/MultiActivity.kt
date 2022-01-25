020798f25f68585f8b1f633132bd4c3a5a1ce5a2
heonseopShin <shinsub6120@gmail.com>
2022-01-26 00:53:29 +0900

    implement Multi function with compose
    description:
    - Scaffold, TextField, Button, Snackbar
    - Navigation

---
+++ app/src/main/java/com/mintcode/compose/MultiActivity.kt
@@ -0,0 +1,54 @@
+package com.mintcode.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import kotlinx.coroutines.launch
+
+
+class MultiActivity : ComponentActivity() {
+    @ExperimentalComposeUiApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val (text, setValue) = remember {
+                mutableStateOf("")
+            }
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            val keyboardController = LocalSoftwareKeyboardController.current
+            Scaffold(
+                scaffoldState = scaffoldState,
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    TextField(
+                        value = text,
+                        onValueChange = setValue
+                    )
+                    Button(onClick = {
+                        keyboardController?.hide()
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Hello $text")
+                        }
+                    }) {
+                        Text("클릭!!")
+                    }
+                }
+            }
+        }
+    }
+}
